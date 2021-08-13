e49ce8b0e1ca491b247b9d5dc8e4d04bd67ada8a
Mainulhasan05 <mdrifatbd5@gmail.com>
2021-08-13 20:21:04 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapp2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv1,iv2,iv3;
+    TextView tv1,tv2,tv3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=
+    }
+}
